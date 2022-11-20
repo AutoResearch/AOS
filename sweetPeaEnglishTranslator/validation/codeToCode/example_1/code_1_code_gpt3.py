d4e9e1295ee4296ef88d22fbb057026194899c66
Younes Strittmatter <younes_strittmatter@brown.edu>
2022-11-20 18:03:45 -0500

    first code to code validation with sour pea sucess with simple stroop

---
+++ sweetPeaEnglishTranslator/validation/codeToCode/example_1/code_1_code_gpt3.py
@@ -0,0 +1,21 @@
+from sweetpea import *
+from sweetpea.primitives import *
+
+### REGULAR FACTORS
+color = factor("color",  ["red", "green", "blue", "yellow"])
+word = factor("word", ["red", "green", "blue", "yellow"])
+### DERIVED FACTORS
+##
+def is_congruency_congruent(word, color):
+    return word == color
+def is_congruency_incongruent(word, color):
+    return not is_congruency_congruent(word, color)
+congruency = factor("congruency", [derived_level("congruent", within_trial(is_congruency_congruent, [word, color])), derived_level("incongruent", within_trial(is_congruency_incongruent, [word, color]))])
+### EXPERIMENT
+constraints = [minimum_trials(48)]
+crossing = [word, congruency]
+design = [color, word, congruency]
+block = fully_cross_block(design, crossing, constraints, False)
+experiments = synthesize_trials_non_uniform(block, 1)
+### END OF EXPERIMENT DESIGN
+save_experiments_csv(block, experiments, 'code_2_sequences/seq')
